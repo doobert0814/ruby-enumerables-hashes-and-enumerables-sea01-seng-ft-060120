830b9b7a2398a73e7d1235b664e2a952b5164c3a
Scot Sexton <rss3509@gmail.com>
2020-05-20 22:40:19 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,7 +8,7 @@
 
 def select_winner(passengers)
   winner = ""
-  passengers.each }|suite, name| 
+  passengers.each { |suite, name| 
   if suite == :suite_a && name.starts_with?("A")
     winner = name
   }
